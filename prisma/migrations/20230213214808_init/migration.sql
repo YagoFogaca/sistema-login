6accd3f0012d46ba31ea3bab03d482fef686eccb
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-13 18:57:39 -0300

    feat: implementação do prisma e conexão com database

---
+++ prisma/migrations/20230213214808_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "authentication" BOOLEAN NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_key" ON "user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
