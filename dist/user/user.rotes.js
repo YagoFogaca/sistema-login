48b22fb9c36fc553075bf59e9895e5028d76d23c
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-14 08:10:18 -0300

    feat: remmoção de arquivos da dist

--- dist/user/user.rotes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRotes = void 0;
-class UserRotes {
-    constructor(userService) {
-        this.userService = userService;
-    }
-}
-exports.UserRotes = UserRotes;
