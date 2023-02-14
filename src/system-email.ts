16521e9f0b4a7d6cc7da497aa2c6bc285f50f7fe
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-14 15:48:02 -0300

    feat: implementação do nodemailer para autenticação de email da aplicação

---
+++ src/system-email.ts
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer';
+
+export function AuthenticationEmail(to: string) {
+    const cod = Math.floor(Math.random() * 89999) + 10000;
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'doe.jhon.fogaca@gmail.com',
+            pass: 'mmtaxfpxcqsxqyin',
+        },
+    });
+
+    const options = {
+        from: 'doe.jhon.fogaca@gmail.com',
+        to: to,
+        subject: 'Autenticação do Email',
+        text: `Você realizou o cadastro na nossa plataforma, agora você precisa autenticar o seu Email.
+        \n\n
+        Seu código de validação é : ${cod}
+        \n\n
+        Obrigado pela atenção! Att Yago Fogaça
+        `,
+    };
+
+    const res = transporter.sendMail(options, function (error, info) {
+        if (error) {
+            console.log(error);
+            return false;
+        } else {
+            console.log('Email enviado com sucesso');
+            return true;
+        }
+    });
+
+    return res;
+}
